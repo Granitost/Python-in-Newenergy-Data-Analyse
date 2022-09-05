d4d6bc15f70c483d8d13199a68dc5d3744211734
Granitost <98318292+Granitost@users.noreply.github.com>
2022-09-05 13:55:32 +0800

    Add files via upload

---
+++ work3-1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 29 13:52:52 2022
+
+@author: zxdft
+"""
+
+def func1():
+    inputObj=input()
+    if inputObj=="H2O":
+        print("water")
+    elif inputObj=="O2":
+        print("oxygen")
+    else:
+        print("No result found")
+func1()
+        
